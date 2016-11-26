779e5c6f33319d46da9cd09bb0d9dc9dab6781b1
Theo96sc <theo_96.sc@outlook.es>
2016-11-26 00:09:38 -0500

    test

---
+++ clsConsultorio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class clsConsultorio
+    {
+        List<clsMedico> medicos = new List<clsMedico>();
+
+
+        public clsMedico obtenerUltimoMedico()
+        {
+            return this.medicos.Last();
+        }
+        public void grabarMedico(clsMedico m)
+        {
+            this.medicos.Add(m);
+        }
+        public clsMedico obtenerMedico(????? ident)
+        {
+            return this.medicos.Where(p => p.id == ident).FirstOrDefault();
+        }
+    }
+}
